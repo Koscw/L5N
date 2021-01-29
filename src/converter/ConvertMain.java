675912fdb7fada31dde6d79e356865ef31dbfd9e
yaroslav <yaroslavfgsf@gmail.com>
2021-01-29 22:37:27 +0200

    add

--- src/converter/ConvertMain.java
+++ src/converter/ConvertMain.java
@@ -6,24 +6,34 @@ import java.io.InputStreamReader;
 
 public class ConvertMain {
     public static void main(String[] args) throws IOException {
+
+        System.out.println("enter the temperature");
+        BufferedReader bufferedReader1 = new BufferedReader(new InputStreamReader(System.in));
+        double celsius = Double.parseDouble(bufferedReader1.readLine());
         System.out.println("Enter the temperature type(kelvin/fahrenheit) in what you want to convert");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String in;
         while (!(in = bufferedReader.readLine()).isEmpty()) {
-            double result;
-            switch(in){
+            double result ;
+            switch (in) {
                 case "kelvin":
-                    Kelvin kelvin = new Kelvin();
-                    kelvin.convert();
+                    result=Converter.kelvin(celsius);
+                    System.out.println(result+" kelvins");
                     break;
                 case "fahrenheit":
-                    Fahrenheit fahrenheit = new Fahrenheit();
-                    fahrenheit.convert();
+                    result =Converter.fahrenheit(celsius);
+                    System.out.println(result+" fahrenheits");
                     break;
-                default :
+                default:
                     System.out.println("Error , try one more time");
 
+
+
             }
+
         }
+
+
+
     }
 }
--- src/converter/Converter.java
+++ src/converter/Converter.java
@@ -1,7 +1,18 @@
 package converter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public interface Converter {
-    void convert() throws IOException;
+
+    public static double kelvin(double celsius){
+        double result = celsius+273.15;
+        return result;
+    }
+    public static double fahrenheit(double celsius){
+        double result =(9/5*celsius)+32;
+        return result;
+    }
+
 }
